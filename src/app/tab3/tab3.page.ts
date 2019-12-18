d56788ba4c8e536d9d9010c267c5e3ecb4ecb8e5
EenPakjeAppelSap <49155413+EenPakjeAppelSap@users.noreply.github.com>
2019-12-18 10:00:10 +0100

    Tab 3 aangepast
    Tab 3 aangepast zodat nu alle vragen voor het aanmaken van een nieuwe recept op 1 pagina staat

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';  
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -8,9 +9,28 @@ import { Router } from '@angular/router';
 })
 export class Tab3Page{
 
-  constructor(private router: Router){}
+  public myForm: FormGroup;
+  private stepCount: number = 1;
+
+  constructor(private router: Router, formBuilder: FormBuilder){
+
+    this.myForm = formBuilder.group({
+      user: ['', Validators.required]
+    });
+  }
   toSecondPage()
   {
     this.router.navigate(['stap2']);
   }
+
+  
+  addControl(){
+    this.stepCount++;
+    this.myForm.addControl('step' + this.stepCount, new FormControl('', Validators.required));
+  }
+
+  removeControl(control){
+    this.myForm.removeControl(control.key);
+  }
+
 }
