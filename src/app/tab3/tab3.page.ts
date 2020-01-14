1e7bbf1b7efa8f2871ba31dae51160204c71bf34
EenPakjeAppelSap <49155413+EenPakjeAppelSap@users.noreply.github.com>
2020-01-14 14:19:29 +0100

    Ion alert update

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -52,8 +52,9 @@ export class Tab3Page{
           text: 'Bevestig recept',
           handler: () =>  { 
             console.log('Bevestig recept');
-            this.clearInputs();
             this.uploadRecipe();
+            this.clearInputs();
+            this.uploadDone();
 
           }
         }
@@ -65,8 +66,21 @@ export class Tab3Page{
     console.log(result);
   }
 
+  async uploadDone() {
+    const alert = await this.alertController.create({
+      header: 'Recept',
+      message: 'Recept is aangemaakt',
+      buttons: ['OK'],
+    });
+  
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+
   clearInputs(){
-    this.recipeName = '';
+  this.recipeName = '';
+  this.ingredients = '';
   this.category = '';
   this.mealTime = '';
   this.people='';
