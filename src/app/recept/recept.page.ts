cf8f42cff0d31e8f318b09c3d2f927c3becd08aa
Glenn-Nellen <nellensteijn.g@gmail.com>
2020-01-23 13:06:35 +0100

    update

--- src/app/recept/recept.page.ts
+++ src/app/recept/recept.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { AuthenticationService } from '../shared/authentication.service'
+import { getLocaleExtraDayPeriods } from '@angular/common';
 
 @Component({
   selector: 'app-recept',
@@ -12,10 +15,11 @@ receptid = ''
 recipeName = ''
 public recipe: any[];
 
-  constructor(private firestore: AngularFirestore, private router: Router) {
+  constructor(public afAuth: AngularFireAuth, public authenticationService: AuthenticationService, private firestore: AngularFirestore, private router: Router,  private db: AngularFirestore) {
   }
-
+  user = this.afAuth.auth.currentUser.email
   ngOnInit() {
+    this.recipeName = this.router.getCurrentNavigation().extras.state.recipeName
     this.receptid = this.router.getCurrentNavigation().extras.state.receptid
     this.firestore.collection(`recipe`, ref => ref.where('id', '==', this.receptid)).valueChanges()
     .subscribe(recipeList => { 
@@ -25,6 +29,13 @@ public recipe: any[];
   }
   toArray(answers: object) {
     return Object.keys(answers).map(key => answers[key])
+  }
+  addFavorite() {
+    this.db.collection('favorite').add({
+      recipe_id: this.receptid,
+      user: this.user,
+      name: this.recipeName
+      })
   }
 }
 
